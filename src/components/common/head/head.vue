c47ecea41b91e009fa787e54d5e45ee61e219faa
StanXu <1316239136@qq.com>
2021-03-09 19:48:09 +0800

    更新

---
+++ src/components/common/head/head.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="head">
+    <button type="button" name="woaini">sadasd</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "head"
+}
+</script>
+
+<style scoped>
+.head{
+  font-size: 10px;
+  border: 1px solid #9f8ead;
+  background-color: #c95353;
+  width: 100px;
+  height: 100px;
+}
+</style>
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,15 +1,56 @@
 <template>
   <div>
-    首页
+    <el-container id="header-container" class="container">
+      <el-header id="el-header" class="container">
+
+      </el-header>
+    </el-container>
+    <el-container id="aside-main-container" class="container">
+      <el-aside id="el-aside" class="container">
+
+      </el-aside>
+      <el-main id="el-main" class="container">
+
+      </el-main>
+    </el-container>
   </div>
+
 </template>
 
 <script>
+
 export default {
-name: "Main"
+  name: "Main"
 }
 </script>
 
 <style scoped>
+.container {
+  padding: 0;
+  margin: 0;
+  border: 1px solid blue;
+  background-color: #faf7f7;
+}
 
+#header-container {
+  border: 1px solid blue;
+  height: 60px;
+}
+
+#aside-main-container {
+  border: 1px solid yellow;
+  height: 1000px;
+}
+
+#el-header {
+  border: 1px solid red;
+}
+
+#el-aside {
+  border: 1px solid green;
+}
+
+#el-main {
+  border: 1px solid black;
+}
 </style>
