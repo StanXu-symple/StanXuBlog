41d9e7fa5fc1c0bed28b443de8f4337334db2def
StanXu <1316239136@qq.com>
2021-03-11 19:20:53 +0800

    更新，解决跨域以及post传到后台对象

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
+    <el-form ref="loginForm" :model="user" :rules="rules" label-width="80px" class="login-box">
       <h3 class="login-title">欢迎登录</h3>
       <el-form-item label="账号" prop="username">
-        <el-input type="text" placeholder="请输入账号" v-model="form.username"/>
+        <el-input type="text" placeholder="请输入账号" v-model="user.userName"/>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input type="password" placeholder="请输入密码" v-model="form.password"/>
+        <el-input type="password" placeholder="请输入密码" v-model="user.userPassword"/>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>
+        <el-button type="primary" v-on:click="onSubmit(user)">登录</el-button>
       </el-form-item>
     </el-form>
 
@@ -27,21 +27,23 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "Login",
   data() {
     return {
-      form: {
-        username: '',
-        password: ''
+      user: {
+        userName: '',
+        userPassword: ''
       },
 
       // 表单验证，需要在 el-form-item 元素中增加 prop 属性
       rules: {
-        username: [
+        userName: [
           {required: true, message: '账号不可为空', trigger: 'blur'}
         ],
-        password: [
+        userPassword: [
           {required: true, message: '密码不可为空', trigger: 'blur'}
         ]
       },
@@ -51,8 +53,8 @@ export default {
     }
   },
   methods: {
-    onSubmit(formName) {
-      // 为表单绑定验证功能
+    onSubmit(user) {
+      /*// 为表单绑定验证功能
       this.$refs[formName].validate((valid) => {
         if (valid) {
           // 使用 vue-router 路由到指定页面，该方式称之为编程式导航
@@ -61,6 +63,22 @@ export default {
           this.dialogVisible = true;
           return false;
         }
+      });*/
+      console.log(user);
+      console.log(user.userName);
+      console.log(user.userPassword);
+      let login=JSON.stringify({
+        userName:user.userName,
+        userPassword:user.userPassword
+      });
+      axios.post(('/api/login'),login,{
+        headers:{
+          'Content-Type': 'application/json;charset=utf-8'
+        }
+      }).then(function (res){
+        console.log(res)
+      }).catch(function (error){
+        console.log(error)
       });
     },
     handleClose:function (){
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    devServer:{
+        host:'localhost',
+        port:'8084',
+        proxy:{
+            '/api':{
+                target:'http://localhost:8081',
+                changeOrigin:true
+            }
+        }
+    }
+}
